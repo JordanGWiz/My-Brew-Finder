e4d8c7fc4855b4cd253141d447d13f7b899147b6
Renz <renzlakers24@gmail.com>
2024-05-08 21:23:10 -0700

    updated info.js that currently shows an image derived from yelp's api after digesting the object

--- assets/js/info.js
+++ assets/js/info.js
@@ -1,5 +1,15 @@
 const resultBody = document.getElementById("result-content");
 
+const category = localStorage.getItem('brewInput');
+let categoryQuery;
+
+if (category === 'coffee'){
+    categoryQuery = 'Coffee & Tea';
+}
+else{
+    categoryQuery = 'Beer, Wine & Spirits'
+}
+
 function apiTest(event){
 let API_KEY ="Bearer 2H2SMlwuCiG_ENk1cA4v9BPCZ16BNmEY7esysJNeIrqHdSWYGzwsG0Dx62IdJcXmvSU57_3SJRnUGt7ubLlmLcvHDnvBuEPbbX_3jWTklQBXPL-it0JY6vSAP5c5ZnYx"
     const options = {
@@ -11,9 +21,14 @@ let API_KEY ="Bearer 2H2SMlwuCiG_ENk1cA4v9BPCZ16BNmEY7esysJNeIrqHdSWYGzwsG0Dx62I
 
       };
       
-      fetch('https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=NYC&sort_by=best_match&limit=20', options)
+      fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=NYC&category=${categoryQuery}&sort_by=best_match&limit=3`, options)
         .then(response => response.json())
-        .then(response => console.log(response))
+        .then(response => {console.log(response)
+            console.log(response.businesses[0].image_url);
+            let imageDate = new Image();
+            imageDate.src = response.businesses[0].image_url;
+            resultBody.appendChild(imageDate);
+        })
         .catch(err => console.error(err));
 }
 
