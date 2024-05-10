f4174a811c306ed35a5fa93ccdcf0e2d8ac8b6d5
Renz <renzlakers24@gmail.com>
2024-05-09 20:07:14 -0700

    yelp api call now includes submitted country and city inputs as well as updated form.js to include inputs from info.html

--- assets/js/info.js
+++ assets/js/info.js
@@ -4,16 +4,42 @@ const category = localStorage.getItem('brewInput');
 let categoryQuery;
 const cityInput = localStorage.getItem('cityInput');
 const countryInput = localStorage.getItem('countryInput');
-let locationQuery = `Berlin, Germany`;
-if (category === 'coffee'){
+let locationQuery = `${cityInput}, ${countryInput}`;
+
+
+const searchForm = document.getElementById('search-form');
+
+function searchHandler(event){
+    event.preventDefault();
+    const brewInput = document.getElementById('brew-input').value;
+    const countryInput = document.getElementById('country-input').value;
+    const cityInput = document.getElementById('city-input').value;
+
+    if (!brewInput || !countryInput || !cityInput){
+        alert("Please complete all inputs!");
+        return;
+    }
+    else{
+        localStorage.setItem('brewInput', brewInput);
+        localStorage.setItem('countryInput', countryInput);
+        localStorage.setItem('cityInput', cityInput);
+    }
+
+}
+function categoryHandler(){
+  if (category === 'Coffee'){
     categoryQuery = 'Coffee & Tea';
 }
-else{
+  else{
     categoryQuery = 'Beer, Wine & Spirits';
 }
+}
 
-function apiTest(event){
-let API_KEY ="Bearer 2H2SMlwuCiG_ENk1cA4v9BPCZ16BNmEY7esysJNeIrqHdSWYGzwsG0Dx62IdJcXmvSU57_3SJRnUGt7ubLlmLcvHDnvBuEPbbX_3jWTklQBXPL-it0JY6vSAP5c5ZnYx"
+searchForm.addEventListener('submit', searchHandler);
+
+
+function yelpQuery(event){
+  let API_KEY ="Bearer 2H2SMlwuCiG_ENk1cA4v9BPCZ16BNmEY7esysJNeIrqHdSWYGzwsG0Dx62IdJcXmvSU57_3SJRnUGt7ubLlmLcvHDnvBuEPbbX_3jWTklQBXPL-it0JY6vSAP5c5ZnYx";
     const options = {
         method: 'GET',
         headers: {
@@ -26,7 +52,8 @@ let API_KEY ="Bearer 2H2SMlwuCiG_ENk1cA4v9BPCZ16BNmEY7esysJNeIrqHdSWYGzwsG0Dx62I
       fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=${locationQuery}&categories=${categoryQuery}&sort_by=rating&limit=3`, options)
         .then(response => response.json())
         .then(response => {console.log(response)
-            console.log(response.businesses[0]);
+            console.log(response.businesses[1]);
+            console.log(categoryQuery);
             for ( let data of response.businesses){
                 let imageDate = new Image();
                 imageDate.src = data.image_url;
@@ -37,8 +64,15 @@ let API_KEY ="Bearer 2H2SMlwuCiG_ENk1cA4v9BPCZ16BNmEY7esysJNeIrqHdSWYGzwsG0Dx62I
                 cardBody.appendChild(imageDate);
                 let descriptionBody = document.createElement('div');
                 descriptionBody.setAttribute('class', 'container');
-                let title = document.createElement('h4');
+                let title = document.createElement('a');
                 title.textContent = data.name;
+                if(data.attributes.menu_url){
+                  title.setAttribute('href', data.attributes.menu_url);
+                }
+                else{
+                  title.setAttribute('href', data.url);
+                }
+                
                 descriptionBody.appendChild(title);
                 cardBody.appendChild(descriptionBody);
                 resultBody.append(cardBody);
@@ -47,5 +81,8 @@ let API_KEY ="Bearer 2H2SMlwuCiG_ENk1cA4v9BPCZ16BNmEY7esysJNeIrqHdSWYGzwsG0Dx62I
         .catch(err => console.error(err));
 }
 
-window.addEventListener('load', apiTest);
+$(document).ready(function(){
+  categoryHandler;
+  yelpQuery;
+})
 
